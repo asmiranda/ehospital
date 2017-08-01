a1795bc60eaa85ae7f0d911fef3f6be6c4bbb3e1
asmiranda <niq77avie@yahoo.com>
2017-08-01 20:35:10 +0800

    Initial

---
+++ src/main/java/com/ajb/ehospital/EhospitalApplication.java
@@ -0,0 +1,12 @@
+package com.ajb.ehospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhospitalApplication.class, args);
+	}
+}
